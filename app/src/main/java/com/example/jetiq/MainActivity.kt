22b57d79aebaec069c0a7be0f2bfd260f0de30c4
ignatiysss <ignatiysmuller@gmail.com>
2023-08-25 11:56:57 +0100

    fix toolbar

--- app/src/main/java/com/example/jetiq/MainActivity.kt
+++ app/src/main/java/com/example/jetiq/MainActivity.kt
@@ -1,15 +1,62 @@
 package com.example.jetiq
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+               R.id.action_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_LONG)
+            }
+            drawer.closeDrawer(GravityCompat.START)
+            true
+        }
+
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainer, ScheduleFragment())
         transaction.commit()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
