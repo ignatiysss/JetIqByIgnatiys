40cc55821da3426c6009734f5666b73ea1a41e3b
ignatiysss <ignatiysmuller@gmail.com>
2023-09-07 13:07:40 +0100

    add themes in SettingsFragment

--- app/src/main/java/com/example/jetiq/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/jetiq/fragment/SettingsFragment.kt
@@ -1,12 +1,44 @@
 package com.example.jetiq.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import com.example.jetiq.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "theme_preference") {
+            val themeOption = sharedPreferences?.getString(key, "light")
+            applyTheme(themeOption)
+        }
+    }
+
+    private fun applyTheme(themeOption: String?) {
+        when(themeOption) {
+            "light" -> {
+                // Apply light theme
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            "dark" -> {
+                // Apply dark theme
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
     }
 }
